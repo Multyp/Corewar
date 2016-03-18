f5439f4e52b0656337da86daf3731adf84046f37
Peau Clement <clement.peau@epitech.eu>
2016-03-18 17:26:26 +0000

    strdup

---
+++ src/lib/my_strdup.c
@@ -0,0 +1,25 @@
+/*
+** my_strdup.c for lol in /home/peau_c/rendu/PSU/PSU_2015_tetris
+**
+** Made by Clement Peau
+** Login   <peau_c@epitech.net>
+**
+** Started on  Sat Mar  5 14:00:59 2016 Clement Peau
+** Last update Mon Mar 14 13:16:35 2016 Clement Peau
+*/
+
+#include "tetris.h"
+
+char	*my_strdup(char *str)
+{
+  int	i;
+  char	*dup;
+
+  i = -1;
+  if ((dup = malloc((my_strlen(str) + 1) * sizeof(char))) == NULL)
+    return (NULL);
+  while (str[++i])
+    dup[i] = str[i];
+  dup[i] = 0;
+  return (dup);
+}
