3370c82805600ff498fdc84f28778f3d59ff5663
Peau Clement <clement.peau@epitech.eu>
2016-03-24 23:42:54 +0000

    merge

--- src/compiler/write_action.c
+++ src/compiler/write_action.c
@@ -5,7 +5,7 @@
 ** Login   <peau_c@epitech.net>
 **
 ** Started on  Thu Mar 24 16:25:34 2016 Poc
-** Last update Thu Mar 24 17:44:31 2016 Poc
+** Last update Thu Mar 24 23:07:47 2016 Poc
 */
 
 #include "asm.h"
@@ -18,7 +18,7 @@ int	write_args(t_arg *arg, int fd)
     write_direct();
   else if (arg->type == 3)
     write_indirects();
-    return (0);
+  return (0);
 }
 
 int	write_action(t_action *action, int fd)
@@ -30,12 +30,15 @@ int	write_action(t_action *action, int fd)
     printf("Coding byte-> %x\n", action->coding_byte);
   else
     printf("Coding byte -> 0\n");
+  write(fd , &action->identifier, 1);
   while (action->args[i])
     {
+      printf("action->identifier %d\n", action->identifier);
       printf("action->args->link_name %s\n", action->args[i]->link_name);
       printf("action->args->type %d\n", action->args[i]->type);
       printf("action->args->pos_link = %d\n", action->args[i]->pos_link);
       printf("action->args->value %d\n", action->args[i]->value);
+      write(fd, &action->coding_byte, 1);
       write_args(action->args[i], fd);
       i++;
     }
