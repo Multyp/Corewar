3033c2a52d2a25082204532c1d2acd0dc8fa9b00
Peau Clement <clement.peau@epitech.eu>
2016-03-25 13:01:25 +0000

    merge

--- test.s
+++ test.s
@@ -1,15 +1,12 @@
 	.name "zork"
 	.comment "Just a basic living program"
 
-<<<<<<< HEAD
 l2:
 	sti r1,%:live,%1
 	and r1,%0,r1
 live:	live %1
 	zjmp %:live
-=======
 	ld 34,r3
 	sti r2,r3,r4
 	live %1
 	zjmp %1
->>>>>>> d3d9dfc503a2732b62ea73168119d136e1bcb3bc
