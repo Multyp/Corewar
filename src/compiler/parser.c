ec1d9f95fbf53e216017633544816b202ddc0bdf
Arthur Arnaud <arthur.arnaud@epitech.eu>
2016-03-26 19:56:04 +0000

    fix

--- src/compiler/parser.c
+++ src/compiler/parser.c
@@ -5,7 +5,7 @@
 ** Login   <peau_c@epitech.net>
 **
 ** Started on  Thu Mar 24 12:58:41 2016 Poc
-** Last update Sat Mar 26 18:57:41 2016 Arthur ARNAUD
+** Last update Sat Mar 26 19:33:08 2016 Arthur ARNAUD
 */
 
 #include "asm.h"
@@ -14,8 +14,6 @@ int	parser(t_label *label, t_action *action, t_header *header, char *name)
 {
   int	fd;
 
-  test_label(label);
-  test_action(action);
   if ((fd = write_header(header, name)) == -1)
     return (1);
   while (action && action->instruction)
